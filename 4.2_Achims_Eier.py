f1905b8aea86fcc1a870c945f3de1250a2dd87ab
Actionschnitzel <67635013+actionschnitzel@users.noreply.github.com>
2024-06-02 18:18:47 +0200

    Update 4.2_Achims_Eier.py

--- 4.2_Achims_Eier.py
+++ 4.2_Achims_Eier.py
@@ -1,4 +1,4 @@
-print("Jutta hat doppelt soviele Eier wie Achim. Er besitzt genau 6 Eier. Jutta schenkt Achim die Hälfte ihrer Eier.\n\nFrage: Wieviele Eier hat Achim?")
+print("Jutta hat doppelt so viele Eier wie Achim. Er besitzt genau 6 Eier. Jutta schenkt Achim die Hälfte ihrer Eier.\n\nFrage: Wie viele Eier hat Achim?")
 
 achims_eier = 6
 
@@ -14,4 +14,3 @@ print("\nLösung:\n")
 #print(f"Jutta hat {juttas_eier} {check_plural}")
 
 print(f"Achim hat die Hälfte von Juttas {juttas_eier} Eiern bekommen.\nEs sind genau {juttas_eier / 2 } {check_plural}.\nDas heißt, Achim hat jetzt {achims_eier + juttas_eier / 2} {check_plural}.")
-
