e6abe1d7818f25abddd935b282c836136db4581f
Actionschnitzel <67635013+actionschnitzel@users.noreply.github.com>
2024-06-02 15:23:50 +0200

    Create 4.2_Achims_Eier.py

---
+++ 4.2_Achims_Eier.py
@@ -0,0 +1,17 @@
+print("Jutta hat doppelt soviele Eier wie Achim. Er hat besitzt genau 6 Eier. Jutta schenkt Achim die helfter ihrer Eier.\n\nFrage: Wieviele Eier hat Achim?")
+
+achims_eier = 6
+
+juttas_eier = achims_eier * 2
+
+check_plural = "Ei"
+
+if achims_eier >=2:
+    check_plural = "Eier"
+
+print("\nLösung:\n")
+#print(f"Achim hat {achims_eier} {check_plural}")
+#print(f"Jutta hat {juttas_eier} {check_plural}")
+
+print(f"Achim hat die Helfte von Juttas {juttas_eier} Eiern bekommen.\nEs sind genau {juttas_eier / 2 } {check_plural}.\nDas heißt, Achim hat jetzt {achims_eier + juttas_eier / 2} {check_plural}.")
+
