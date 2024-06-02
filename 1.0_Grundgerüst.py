b815a3d7650d7bf7214eea498267dd75b8909309
Actionschnitzel <67635013+actionschnitzel@users.noreply.github.com>
2024-06-02 15:17:05 +0200

    Create 1.0_Grundgerüst.py

---
+++ 1.0_Grundgerüst.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import tkinter as tk
+
+# Erstelle das Hauptfenster
+root = tk.Tk()
+root.title("Neofetch-Tk")
+root.geometry("600x400")
+
+# Starte die Hauptschleife
+root.mainloop()
